f2cbf2f1a66c628cff6f67597139922a9d3c813e
varsha-dev1 <71543472+varsha-dev1@users.noreply.github.com>
2020-10-19 12:43:05 +0530

    Add files via upload

---
+++ day_4/ArrayWithUserInput.java
@@ -0,0 +1,25 @@
+//Day_4 Assignment_3
+
+package letsUpgrade;
+import java.util.Scanner;
+public class ArrayWithUserInput {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] array = new int[5];
+		int sum=0;
+		System.out.println("Enter elements of array: ");
+		for(int i=0;i<5;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		for(int i=0;i<5;i++)
+		{
+			sum=sum+array[i];
+		}
+		System.out.println("Total of all elements of array is: "+sum);
+
+
+	}
+
+}
